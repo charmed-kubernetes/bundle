152ad254facfee17ec21cde9f10872578387b47c
Charles Butler <chuck@dasroot.net>
2016-09-24 16:46:40 +0000

    WIP

---
+++ tests/20-topdown.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import amulet
+import os
+import time
+import unittest
+import yaml
+
+SECONDS_TO_WAIT = 1200
+
+
+class TestLocalBundle(unittest.TestCase):
+    ''' Test Local Bundle tests the top down view of the kubernetes deployment,
+        and will perform baseline verification that all deployed
+        applications have performed their role in the setup'''
+
+    @classmethod
+    def setUpClass(cls):
+        """Read the bundle in and deploy the bundle."""
+        # Get the relative bundle path from the environment variable.
+        cls.bundle = os.getenv('BUNDLE', 'local.yaml')
+        # Create a path to the bundle based on this file's location.
+        cls.bundle_path = os.path.join(os.path.dirname(__file__),
+                                       '..',
+                                       cls.bundle)
+        # Normalize the path to the bundle.
+        cls.bundle_path = os.path.abspath(cls.bundle_path)
+
+        print('Deploying bundle: {0}'.format(cls.bundle_path))
+
+        cls.deployment = amulet.Deployment()
+        with open(cls.bundle_path, 'r') as bundle_file:
+            contents = yaml.safe_load(bundle_file)
+            cls.deployment.load(contents)
+
+        # Allow some time for Juju to provision and deploy the bundle.
+        cls.deployment.setup(timeout=SECONDS_TO_WAIT)
+        # Wait for the system to settle down.
+        cls.deployment.sentry.wait()
+
+        cls.kubemaster = cls.deployment.sentry['kubernetes-master']
+        cls.worker = cls.deployment.sentry['kubernetes-worker']
+        cls.etcd = cls.deployment.sentry['etcd']
+        cls.easyrsa = cls.deployment.sentry['easyrsa']
+        cls.loadbalancer = cls.deployment.sentry['kubeapi-load-balancer']
+
+    def test_things(self):
+        print('nope')
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/attach_resources.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# This is for deploying local charms, and attaching local resources. How
+# you obtain these resources is up to the operator
+
+juju attach kubernetes-master kubernetes=~/resources/kubernetes-master.tar.gz
+juju attach kubernetes-worker kubernetes=~/resources/kubernetes-worker.tar.gz
+juju attach flannel flannel=~/resources/flannel-v0.6.1-linux-amd64.tar.gz
+juju attach easyrsa easyrsa=~/resources/EasyRSA-3.0.1.tgz
+
+
